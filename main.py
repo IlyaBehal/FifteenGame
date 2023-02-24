7f6d7fc36248e258f4246ed5423bc8cd5b98dab8
Illko <illabegal@gmail.com>
2023-02-24 18:59:35 +0200

    Add draw function

--- main.py
+++ main.py
@@ -12,4 +12,28 @@ c = Canvas(root, width=BOARD_SIZE * SQUARE_SIZE,
            height=BOARD_SIZE * SQUARE_SIZE, bg='#808080')
 c.pack()
 
+
+def draw_board():
+    c.delete('all')  # clean all
+    for i in range(BOARD_SIZE):
+        for j in range(BOARD_SIZE):
+            index = str(board[BOARD_SIZE * i + j])
+            if index != str(EMPTY_SQUARE):
+                c.create_rectangle(j * SQUARE_SIZE, i * SQUARE_SIZE,
+                                   j * SQUARE_SIZE + SQUARE_SIZE,
+                                   i * SQUARE_SIZE + SQUARE_SIZE,
+                                   fill='#43ABC9',
+                                   outline='#FFFFFF')
+                c.create_text(j * SQUARE_SIZE + SQUARE_SIZE / 2,
+                              i * SQUARE_SIZE + SQUARE_SIZE / 2,
+                              text=index,
+                              font="Arial {} italic".format(int(SQUARE_SIZE / 4)),
+                              fill='#FFFFFF')
+
+
+board = list(range(1, EMPTY_SQUARE + 1))
+correct_board = board[:]
+shuffle(board)
+
+
 root.mainloop()
